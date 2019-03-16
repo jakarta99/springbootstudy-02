ee8e561dcc26d92882631b35b2c3d3818040b48d
GaryLee <jakarta99@gmail.com>
2019-03-16 15:03:55 +0800

    Web entrypoint.

---
+++ src/main/java/dev/softleader/springbootstudy/course02/WebSpringBootApplication.java
@@ -0,0 +1,14 @@
+package dev.softleader.springbootstudy.course02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(WebSpringBootApplication.class, args);
+
+	}
+}
